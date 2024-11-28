913107a0185a9d4a247b023d243454ed1f7c6f45
Firas <firasguendouzz@gmail.com>
2024-11-28 17:55:51 +0100

    updates

--- baba-jan-server/server.js
+++ baba-jan-server/server.js
@@ -4,7 +4,6 @@ const connectDB = require('./src/config/db');
 const loadEnv = require('./src/config/env');
 const startTelegramBot = require('./src/config/telegramBot');
 const colors = require('colors');
-const ngrok = require('ngrok');
 
 // Load Environment Variables
 loadEnv();
@@ -21,19 +20,7 @@ const server = http.createServer(app);
 // Define PORT
 const PORT = process.env.PORT || 5000;
 
-// Start the server and expose it via ngrok
-server.listen(PORT, async () => {
-  console.log(colors.green(`Server is running on http://localhost:${PORT}`));
-  console.log(colors.cyan(`Environment: ${process.env.NODE_ENV || 'development'}`));
 
-  // Expose the local server through ngrok for remote access
-  try {
-    const url = await ngrok.connect(PORT);
-    console.log(colors.magenta(`Ngrok is forwarding to: ${url}`));
-  } catch (err) {
-    console.error(colors.red('Error starting ngrok:', err));
-  }
-});
 
 // Handle Uncaught Exceptions
 process.on('uncaughtException', (err) => {
